8f808490ac708fc1fb20c61859b5219498b4dca5
kohlakos <kohlakos>
2025-01-17 21:10:00 +0100

    adding install.sh for one-line git install

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+INSTALL_DIR="$HOME/.gcptree"
+
+if [ ! -d "$INSTALL_DIR" ]; then
+    git clone https://github.com/khelmric/gcptree.git "$INSTALL_DIR"
+else
+    echo "Directory $INSTALL_DIR already exists. Pulling latest changes."
+    cd "$INSTALL_DIR" && git pull
+fi
+
+chmod +x "$INSTALL_DIR/gcptree.sh"
+
+mkdir -p "$INSTALL_DIR/bin"
+
+ln -sf "$INSTALL_DIR/gcptree.sh" "$INSTALL_DIR/bin/gcptree"
+
+if ! grep -q "$INSTALL_DIR/bin" <<< "$PATH"; then
+    echo "export PATH=\"\$PATH:$INSTALL_DIR/bin\"" >> "$HOME/.bashrc"
+    echo "export PATH=\"\$PATH:$INSTALL_DIR/bin\"" >> "$HOME/.zshrc"
+    echo "export PATH=\"\$PATH:$INSTALL_DIR/bin\"" >> "$HOME/.profile"
+    echo "Added $INSTALL_DIR/bin to PATH. Please run 'source ~/.bashrc' or 'source ~/.zshrc' or 'source ~/.profile' to update your PATH."
+else
+    echo "$INSTALL_DIR/bin is already in your PATH."
+fi
+
+echo "Installation complete. You can now use the 'gcptree' command."
+echo ""
+echo "gcptree <ORGANIZATION_ID OR FOLDER_ID> [-v]"
+echo ""
